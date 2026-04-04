22fd30ddc78a20a89535878c8ccc6a7ab5f690fc
JamesSerenio <sereniojames363@gmail.com>
2026-04-04 19:56:20 +0800

    fix seat number 8A

--- lib/pages/Seat.dart
+++ lib/pages/Seat.dart
@@ -109,7 +109,7 @@ class _SeatPageState extends State<SeatPage> with TickerProviderStateMixin {
       ),
       const SeatPin(id: '9', label: '9', x: 28, y: 39.5, kind: PinKind.seat),
 
-      const SeatPin(id: '8A', label: '8A', x: 42, y: 39.5, kind: PinKind.seat),
+      const SeatPin(id: '8A', label: '8A', x: 46, y: 40, kind: PinKind.seat),
       const SeatPin(id: '8B', label: '8B', x: 46, y: 44.5, kind: PinKind.seat),
 
       const SeatPin(id: '7A', label: '7A', x: 58, y: 39.7, kind: PinKind.seat),
